65d72432d16b31e270e392e79c96d288b862d51d
Robert J Nagel <rjnagel64@gmail.com>
2017-12-10 09:58:00 -0600

    Refactor collision detection
    Tweak the collision detection to fit a bit better with using an
    image for the bullet. I also got rid of bulletCheck, because it
    doesn't make sense to check for collisions every millisecond when
    the bullet only moves every 32 milliseconds.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,8 +1,6 @@
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Enemy extends JPanel {
@@ -17,21 +15,32 @@ public class Enemy extends JPanel {
 		this.y = y;
 		this.rightToLeft = rightToLeft;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
+
 	public int getX() {
 		return x;
 	}
-	
+
+	public boolean collides(int x, int y, int width, int height) {
+		if (this.x < x + width &&
+				this.x + this.image.getWidth(null) > x &&
+				this.y < y + height &&
+				this.y + this.image.getHeight(null) > y) {
+			return true;
+		}
+		return false;
+	}
+
 	public void update() {
 	}
-	
+
 	@Override
 	public void paintComponents(Graphics g) {
 		super.paintComponents(g);
-		
+
 		g.drawImage(image, x, y, null);
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -15,37 +15,42 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Game extends JPanel implements ActionListener {
-	private static final int MS_PER_FRAME = 32;
-	private static final int MOVE_AMOUNT = 32;
-	private static final int ENEMY_DELAY = 250;
-	private static final int SPAWN_DELAY = 2000;
+	private static final int POINTS_PER_ENEMY = 100;
+	private static final int POINTS_TO_WIN = 1000;
 	private static final int MAX_ENEMIES = 7;
 	
+	private static final int MOVE_AMOUNT = 32;
+	
+	private static final int MS_PER_FRAME = 32;
+	
+	private static final int ENEMY_DELAY = 250;
+	private static final int SPAWN_DELAY = 2000;
+	
 	private int score = 0;
+	
 	private int bulletX = -32;
 	private int bulletY = -32;
 	private boolean bulletMove = false;
 	private boolean bulletActive = false;
 	private Image bulletImage;
-	
+
 	private Timer gameTimer;
 	private Timer enemyTimer;
 	private Timer enemySpawnTimer;
-	private Timer bulletCheck;
 
 	private Ship ship;
 	private List<Enemy> enemies;
-	
+
 	public Game() {
 		ImageIcon ii = new ImageIcon("bullet.png");
 		bulletImage = ii.getImage();
-		
+
 		ship = new Ship();
 		enemies = new ArrayList<Enemy>();
-		
+
 		gameTimer = new Timer(MS_PER_FRAME, this);
 		gameTimer.start();
-		
+
 		enemyTimer = new Timer(ENEMY_DELAY, new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				for (Enemy e : enemies) {
@@ -54,45 +59,7 @@ public class Game extends JPanel implements ActionListener {
 			}
 		});
 		enemyTimer.start();
-		
-		// this will check for win/loss/and hit collision
-		// it will also update the score in the top right corner
-		bulletCheck = new Timer(1, new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				
-				int yUpper = bulletY + 10;
-				int yLower = bulletY - 10;
-				int xLeft = bulletX - 30;
-				int xRight = bulletX + 30;
-				for(int i = 0; i < enemies.size(); i++) {
-					int Y = (enemies.get(i)).getY(); 
-					int X = (enemies.get(i)).getX(); 
-					if (Y <= yUpper && Y >= yLower && X <= xRight && X >= xLeft) {
-						enemies.remove(i);
-						score += 100;
-						System.out.println("hit");
-					}
-					
-					if(Y >= 400) {
-						bulletCheck.stop();
-						enemySpawnTimer.stop();
-						gameTimer.stop();
-						enemyTimer.stop();
-						System.out.println("you lose");
-					}
-					
-				}
-				if(score >= 1000) {
-					bulletCheck.stop();
-					enemySpawnTimer.stop();
-					gameTimer.stop();
-					enemyTimer.stop();
-					System.out.println("you win");
-				}
-			}
-		});
-		bulletCheck.start();
-		
+
 		enemySpawnTimer = new Timer(SPAWN_DELAY, new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				if (enemies.size() < MAX_ENEMIES) {
@@ -128,7 +95,7 @@ public class Game extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// here is the action performed for the bullet shot
+		// Update the bullet's position.
 		if (bulletMove && bulletActive){
   			bulletY -= 15;
   			if (bulletY <= -50){
@@ -137,27 +104,50 @@ public class Game extends JPanel implements ActionListener {
   			}
   		}
 		
-		/*for(int i = 0; i < enemies.size(); i++) {
-			if(bulletY == (enemies.get(i)).getY()  ) {
-				System.out.println("hi");
+		// Check for bullet collisions.
+		for (int i = 0; i < enemies.size(); i++) {
+			Enemy e = enemies.get(i);
+
+			// If a collision occurs, delete the enemy and increment the score.
+			if (e.collides(
+					bulletX,
+					bulletY,
+					bulletImage.getWidth(null),
+					bulletImage.getHeight(null))) {
+
 				enemies.remove(i);
+				score += POINTS_PER_ENEMY;
+				System.out.println("AABB: hit");
+			
+			// Otherwise, if this enemy has fallen off the bottom of the screen,
+			// The game has been lost.
+			} else if (e.getY() >= CrazyBullets.SCREEN_HEIGHT) {
+				enemySpawnTimer.stop();
+				gameTimer.stop();
+				enemyTimer.stop();
+				System.out.println("you lose");
 			}
-		} */
-		
-		// TODO: Check if any enemy has reached the bottom of the screen. If so,
-		// the game has been lost.
-		
+		}
+
+		// The game is won when sufficient points have been earned.
+		if (score >= POINTS_TO_WIN) {
+			enemySpawnTimer.stop();
+			gameTimer.stop();
+			enemyTimer.stop();
+			System.out.println("you win");
+		}
+
 		repaint();
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		g.drawImage(bulletImage, bulletX, bulletY, null);
 
 		ship.paintComponent(g);
-		
+
 		for (Enemy e : enemies) {
 			e.paintComponents(g);
 		}
@@ -167,14 +157,16 @@ public class Game extends JPanel implements ActionListener {
 		@Override
 		public void keyPressed(KeyEvent e) {
 			int key = e.getKeyCode();
-			
+
 			if (key == KeyEvent.VK_LEFT) {
 				ship.shift(-MOVE_AMOUNT);
 			} else if (key == KeyEvent.VK_RIGHT) {
 				ship.shift(MOVE_AMOUNT);
 			} else if (key == KeyEvent.VK_SPACE) { 
-				if(bulletActive == false){
-	  				bulletX = (ship.getPos()) + 73;
+				if (bulletActive == false) {
+					// 72 is half the width of the ship minus half the width of
+					// the bullet. This centers the bullet above the ship.
+	  				bulletX = (ship.getPos()) + 72;
 	  				bulletY = ship.getPosY();
 	  			}
 	  			bulletMove = true;
